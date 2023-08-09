18dd117f0440193bf6c6c1aeb898d16e7f9ceabc
Hyago Colares <58047846+Hyagocolares@users.noreply.github.com>
2023-08-09 17:42:05 -0300

    Update user.js
    Atualizando algumas funcionalidades

--- user.js
+++ user.js
@@ -7,34 +7,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    const enteredAdminPassword = document.getElementById('adminPassword').value;
 
     try {
-      const response = await fetch('https://hcimports.hyagocolares.repl.co/users', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ username, password })
-      });
+      const adminPasswordResponse = await fetch('https://hcimports.hyagocolares.repl.co/admin-password');
+      const adminPasswordData = await adminPasswordResponse.json();
 
-      const data = await response.json();
+      if (adminPasswordResponse.ok) {
+        const adminPassword = adminPasswordData.mySecret; // Ajuste para o nome correto da variável no JSON
 
-      if (response.ok) {
-        message.textContent = data.message;
-        message.style.color = 'green';
+        if (enteredAdminPassword === adminPassword) {
+          try {
+            const registerResponse = await fetch('https://hcimports.hyagocolares.repl.co/register', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ username, password, adminPasswordEntered: enteredAdminPassword })
+            });
+
+            const registerData = await registerResponse.json();
+
+            if (registerResponse.ok) {
+              message.textContent = 'Usuário criado com sucesso.';
+              message.style.color = 'green';
+            } else {
+              message.textContent = 'Erro ao criar usuário.';
+              message.style.color = 'red';
+            }
+          } catch (error) {
+            console.error('Erro ao fazer a solicitação:', error);
+            message.textContent = 'Erro ao criar usuário.';
+            message.style.color = 'red';
+          }
+        } else {
+          message.textContent = 'Senha de administrador incorreta. Não é possível criar o usuário.';
+          message.style.color = 'red';
+        }
       } else {
-        message.textContent = 'Erro ao criar usuário';
-        message.style.color = 'red';
+        console.error('Erro ao obter a senha do administrador');
       }
     } catch (error) {
       console.error('Erro ao fazer a solicitação:', error);
-      message.textContent = 'Erro ao criar usuário';
+      message.textContent = 'Erro ao criar usuário.';
       message.style.color = 'red';
     }
   });
 
-    const entrarButton = document.getElementById('entrarButton');
+  const entrarButton = document.getElementById('entrarButton');
   entrarButton.addEventListener('click', () => {
-    window.location.href = './index.html'; // Redireciona para a página de entrar
+    window.location.href = './index.html'; // Redireciona para a página de login
   });
 });
