8f7a89b1896ec6c5d98840966a20dbcfceada937
Hyago Colares <58047846+Hyagocolares@users.noreply.github.com>
2023-08-09 13:23:08 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.getElementById('loginForm');
+  const usernameInput = document.getElementById('username');
+  const passwordInput = document.getElementById('password');
+  const messageDiv = document.getElementById('message');
+
+  loginForm.addEventListener('submit', async (event) => {
+    event.preventDefault(); // Previne o comportamento padrão de envio do formulário
+
+    const username = usernameInput.value;
+    const password = passwordInput.value;
+
+    try {
+      const response = await fetch('https://hcimports.hyagocolares.repl.co/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+
+      const data = await response.json();
+
+if (response.ok) {
+        const token = data.token;
+        localStorage.setItem('token', token);
+        window.location.href = './product.html'; // Redireciona para a página protegida após o login
+      } else {
+        messageDiv.textContent = 'Credenciais inválidas. Por favor, tente novamente.';
+      }
+    } catch (error) {
+      console.error('Erro ao fazer login:', error);
+      messageDiv.textContent = 'Erro ao fazer login. Por favor, tente novamente mais tarde.';
+    }
+  });
+
+  const cadastroButton = document.getElementById('cadastroButton');
+  cadastroButton.addEventListener('click', () => {
+    window.location.href = './user.html'; // Redireciona para a página de cadastro
+  });
+});
---
+++ user.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const createForm = document.getElementById('createForm');
+  const message = document.getElementById('message');
+
+  createForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+      const response = await fetch('https://hcimports.hyagocolares.repl.co/users', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password })
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        message.textContent = data.message;
+        message.style.color = 'green';
+      } else {
+        message.textContent = 'Erro ao criar usuário';
+        message.style.color = 'red';
+      }
+    } catch (error) {
+      console.error('Erro ao fazer a solicitação:', error);
+      message.textContent = 'Erro ao criar usuário';
+      message.style.color = 'red';
+    }
+  });
+
+    const entrarButton = document.getElementById('entrarButton');
+  entrarButton.addEventListener('click', () => {
+    window.location.href = './index.html'; // Redireciona para a página de entrar
+  });
+});
